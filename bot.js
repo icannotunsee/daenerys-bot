90db8b92c99f60d89ffcc3f6b4a03b27d9348f52
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2020-09-06 17:20:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,5 +70,3 @@ client.on('message', msg => {
       msg.reply('eyes are organs of the visual system. They provide animals with vision, the ability to receive and process visual detail, as well as enabling several photo response functions that are independent of vision. Eyes detect light and convert it into electro-chemical impulses in neurons.')
     }
   });
-
-  client.login(process.env.DISCORD_TOKEN);
