f2ef91c5513373eb87cca223cc957970f6fcb4a4
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2020-09-13 19:35:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('unsee content', { type: 'WATCHING' });
+    client.user.setActivity('Game of Thrones', { type: 'STREAMING' });
 });
 
 
